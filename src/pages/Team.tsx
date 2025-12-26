9298d819f1d73de2e85bc1ab25019f4699f5fdd1
Himeth Randiv Dharmapriya <randivdharmapriya88@gmail.com>
2025-12-26 18:53:36 +0530

    dhhd

--- src/pages/Team.tsx
+++ src/pages/Team.tsx
@@ -75,8 +75,8 @@ const Team = () => {
             </div>
           </AnimatedSection>
 
-          {/* Team Grid */}
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-12 max-w-6xl mx-auto">
+          {/* Team Grid - 2 columns on mobile, 3 on desktop */}
+          <div className="grid grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8 lg:gap-12 max-w-6xl mx-auto">
             {teamMembers.map((member, index) => (
               <MemberCard key={member.name} member={member} index={index} />
             ))}
